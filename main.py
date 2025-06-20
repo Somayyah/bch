0d52d112a185dba079b1907fcf13e59b320a15fc
watari <watari@techwebunraveled.xyz>
2025-06-20 19:05:42 +0300

    Replace with test/inserted-text.txt

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
 def main():
-    print("Hello from bch!")
+    if len(sys.argv) != 4:
+        print("Usage: python bch.py <file> <offset> <hex-bytes>")
+        sys.exit(1)
+
+    file = sys.argv[1]
+    offset = sys.argv[2]
+    bytes_arg = sys.argv[3]
+
+    with open(file, 'rb') as f:
+        f_content = f.read()
+        print(f_content)
 
 
 if __name__ == "__main__":
